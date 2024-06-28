acb5df2e94e2ea1b1787dea4d0a3dc769a27a206
LuisL <dev.eng.luisluna@gmail.com>
2024-06-28 19:43:01 +0000

    seguimos

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
+import { useHistory } from "react-router";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+
 
 
 
@@ -13,39 +15,23 @@ export const Login = () => {
     /*Controler Component*/
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const history = useHistory();
     const token = sessionStorage.getItem("token");
+    console.log("This is your token", token);
 
     const handleClick = () => {
-        const opts = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
-        };
-
-        fetch('https://3001-rdluisl-pruebabackend-8w3ao2j7exp.ws-us114.gitpod.io/api/token', opts)
-            .then(resp => {
-                if (resp.status === 200) return resp.json();
-                else alert("There has been some error")
-            })
-            .then(data => {
-                console.log("this came from the backend", data)
-                sessionStorage.setItem("token", data.access_token);
-            })
-            .catch(error => {
-                console.error("there was an error!!, error", error);
-            })
+        actions.Login(email, password).then(() => {
+            history.push("/")
+        })
     }
 
     return (
         <div className="text-center mt-5">
             <h1>Login</h1>
             <div>
-                {(token && token != "" && token != undefined) ? "You are logged in with this token: " + token :
+                {(token && token != "" && token != undefined) ? (
+                    "You are logged in with this token: " + token
+                ) : (
                     <div>
                         <input type="test" placeholder="escribe el email" value={email} onChange={(e) => setEmail(e.target.value)} />
                         {/* el onChange captura el evento ya que es cualquier cosa que cambie el estado inicial que en este caso es null */}
@@ -58,7 +44,7 @@ export const Login = () => {
                         {/* el onChange captura el evento ya que es cualquier cosa que cambie el estado inicial que en este caso es null */}
                         <button onClick={handleClick}> Login</button>
                     </div>
-                }
+                )}
             </div >
 
         </div >
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -21,34 +21,65 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			login: async (email, password) => {
+				const handleClick = () => {
+					const opts = {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json"
+						},
+						body: JSON.stringify({
+							email: email,
+							password: password
+						})
+					};
+
+
+					try {
+						const resp = await fetch('https://3001-rdluisl-pruebabackend-8w3ao2j7exp.ws-us114.gitpod.io/api/token', opts)
+						if (resp.status !=== 200) {
+							alert("There has been some error");
+							return false;
+						}
+
+						const data = await resp.json();
+						console.log("this came from backend", data);
+						sessionStorage.setItem("token", data.access_token);
+						return true;
+					}
+					catch (error) {
+						console.error("There has been an error login in")
+					}
 				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+				getMessage: async () => {
+					try {
+						// fetching data from the backend
+						const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+						const data = await resp.json()
+						setStore({ message: data.message })
+						// don't forget to return something, that is how the async resolves
+						return data;
+					} catch (error) {
+						console.log("Error loading message from backend", error)
+					}
+				},
+					changeColor: (index, color) => {
+						//get the store
+						const store = getStore();
 
-				//reset the global store
-				setStore({ demo: demo });
+						//we have to loop the entire demo array to look for the respective index
+						//and change its color
+						const demo = store.demo.map((elm, i) => {
+							if (i === index) elm.background = color;
+							return elm;
+						});
+
+						//reset the global store
+						setStore({ demo: demo });
+					}
 			}
-		}
+		};
 	};
-};
 
-export default getState;
+	export default getState;
